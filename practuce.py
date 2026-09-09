82dd1f254b62e9a366b6b628c4924435c380af1d
Adrika Thakur <adrikathakur06@gmail.com>
2026-09-10 02:19:21 +0530

    conditional statement if elif else practice

---
+++ practuce.py
@@ -0,0 +1,9 @@
+
+
+#write a programe to check if the number is multiple of 7 or not.
+
+user = int(input("enter a number"))
+if (user%7 == 0):
+    print("yes, the multiple of 7 is", user)
+else:
+    print ("no") 
